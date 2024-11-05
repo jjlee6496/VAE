f8e70a5dc3cbd27bc3583f58a0d1b4416edfe46b
jjlee6496 <ljj3499@gmail.com>
2024-11-05 10:12:42 +0900

    🐛 migrate to PyTorch Lightning 2.x

---
+++ tests/test_vae.py
@@ -0,0 +1,27 @@
+import torch
+import unittest
+from models import VanillaVAE
+from torchsummary import summary
+
+class TestVAE(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.model = VanillaVAE(3, 10)
+    
+    def test_summary(self):
+        print(summary(self.model, (3, 64, 64), device='cpu'))
+    
+    def test_forward(self):
+        x = torch.randn(16, 3, 64, 64)
+        y = self.model(x)
+        print("Model Output size:", y[0].size())
+    
+    def test_loss(self):
+        x = torch.randn(16, 3, 64, 64)
+        
+        result = self.model(x)
+        loss = self.model.loss_function(*result, M_N = 0.005)
+        print(loss)
+        
+    if __name__ == '__main__':
+        unittest.main()
